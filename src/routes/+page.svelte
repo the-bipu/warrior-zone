35e3e0953924615b54ae72e7dcfebe1dc8b904a2
The Bipu <127449591+the-bipu@users.noreply.github.com>
2024-11-30 14:32:58 +0530

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,11 @@
   import { onMount } from "svelte";
   import { theme } from "../stores/theme";
 
+  import { Icon } from "svelte-icons-pack";
+  import { AiTwotoneHeart } from "svelte-icons-pack/ai";
+  import { BiBookmark } from "svelte-icons-pack/bi";
+  import { BiComment } from "svelte-icons-pack/bi";
+
   function toggleTheme() {
     theme.update((current) => (current === "light" ? "dark" : "light"));
   }
@@ -33,34 +38,54 @@
     {:else if posts.length === 0}
       <p>Loading...</p>
     {:else}
-      <div class="flex flex-col w-auto gap-4">
+      <div class="flex flex-row flex-wrap w-auto gap-4">
         {#each posts as item}
-          <div class="border border-dashed border-black rounded-2xl shadow bg-white p-4">
-            <h2>{item.username}</h2>
-            <p><strong>Confession:</strong> {item.confession}</p>
-            <p><strong>Type:</strong> {item.type}</p>
-            <p><strong>Likes:</strong> {item.likes}</p>
-            <p><strong>Saves:</strong> {item.saves}</p>
+          <div
+            class="border border-dashed border-black rounded-xl shadow bg-white p-4 relative"
+          >
+            <h2 class="capitalize">By, {item.username}</h2>
+            <div class="mt-2 mb-6">{item.confession}</div>
 
-            <p><strong>Comments:</strong></p>
-            <ul>
-              {#each item.comments as comment}
-                <li>
-                  <strong>{comment.username}:</strong>
-                  {comment.message}
-                  {#if comment.replies.length > 0}
-                    <ul>
-                      {#each comment.replies as reply}
-                        <li>
-                          <strong>{reply.username}:</strong>
-                          {reply.message}
-                        </li>
-                      {/each}
-                    </ul>
-                  {/if}
-                </li>
-              {/each}
-            </ul>
+            <span class="absolute right-0 top-0 px-3 py-1">{item.type}</span>
+
+            <div class="flex flex-row gap-4">
+              <div class="flex flex-col items-center justify-center">
+                <Icon src={AiTwotoneHeart} className="w-6 h-6" />
+                {item.likes}
+              </div>
+
+              <div class="flex flex-col items-center justify-center">
+                <Icon src={BiBookmark} className="w-6 h-6" />
+                {item.saves}
+              </div>
+
+              <div class="flex flex-col items-center justify-center">
+                <Icon src={BiComment} className="w-6 h-6" />
+                {item.comments.length}
+              </div>
+            </div>
+
+            <div>
+              <p><strong>Comments:</strong></p>
+              <ul>
+                {#each item.comments as comment}
+                  <li>
+                    <strong>{comment.username}:</strong>
+                    {comment.message}
+                    {#if comment.replies.length > 0}
+                      <ul>
+                        {#each comment.replies as reply}
+                          <li>
+                            <strong>{reply.username}:</strong>
+                            {reply.message}
+                          </li>
+                        {/each}
+                      </ul>
+                    {/if}
+                  </li>
+                {/each}
+              </ul>
+            </div>
 
             <p>
               <strong>Created At:</strong>
