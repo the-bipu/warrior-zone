4377e2f28ce241a26c52c8a8f6a2f5de01af625d
The Bipu <127449591+the-bipu@users.noreply.github.com>
2024-12-28 20:15:40 +0530

    create contact page
    added dummy contact page

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,18 @@
+<svelte:head>
+  <title>About</title>
+  <meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="text-column">
+  <h1 class="font-medium text-2xl">Dummy Contact Page</h1>
+
+  <div class="text-center mt-2">
+    This is a <a href="https://svelte.dev/docs/kit">SvelteKit</a> app. I created
+    it for people who want to share their terrible experiences—whether about their
+    day or any situation. Initially, I made this app for myself when I was having
+    a horrible day and regretted being too shy to express my feelings. Then, I realized
+    I wasn't the only one who has faced such moments, so I decided to make it public.
+    Now, anyone can share their experiences here, and the best part is that it's
+    completely anonymous—no one is watching you.
+  </div>
+</div>
