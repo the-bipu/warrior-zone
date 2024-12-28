ba65b5a9120e891d436ccda29eb9314bb5a9cbf7
The Bipu <127449591+the-bipu@users.noreply.github.com>
2024-12-28 20:01:12 +0530

    added .svelte-it folder

---
+++ .svelte-kit/generated/client/nodes/4.js
@@ -0,0 +1,3 @@
+import * as universal from "../../../../src/routes/confession/+page.ts";
+export { universal };
+export { default as component } from "../../../../src/routes/confession/+page.svelte";
