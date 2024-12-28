3b692c8a11a3e4892cc14c28862eb3cf17f19a9b
The Bipu <127449591+the-bipu@users.noreply.github.com>
2024-12-28 17:06:11 +0530

    update layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,11 +9,11 @@
 <div class={`flex flex-col min-h-screen ${$theme == 'dark' ? 'bg-[#1a1a1a] text-[#eee]' : 'bg-[#eaeaea] text-[#1a1a1a]'}`}>
 	<Header />
 
-	<main class="flex-1 flex flex-col p-4 w-full">
+	<main class="flex-1 flex flex-col p-4 w-full bg-anime">
 		{@render children()}
 	</main>
 
-	<footer class="flex flex-col justify-center items-center p-3">
+	<footer class="absolute bottom-4 right-4 text-white flex flex-col justify-center items-center p-3">
 		<p>
 			visit <a href="https://github.com/the-bipu/">the-bipu</a> for more help.
 		</p>
