d0f2214e528b558e9651d3efee70b427d42e3192
The Bipu <127449591+the-bipu@users.noreply.github.com>
2024-12-29 15:34:12 +0530

    update pages 🐦‍🔥
    fixed the about the confession pages, with new data and ui of cards fixed

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -3,16 +3,20 @@
   <meta name="description" content="About this app" />
 </svelte:head>
 
-<div class="text-column">
-  <h1 class="font-medium text-2xl">About this app</h1>
+<div class="flex flex-col items-center justify-start w-full min-h-screen">
 
-  <div class="text-center mt-2">
-    This is a <a href="https://svelte.dev/docs/kit">SvelteKit</a> app. I created
-    it for people who want to share their terrible experiences—whether about their
-    day or any situation. Initially, I made this app for myself when I was having
-    a horrible day and regretted being too shy to express my feelings. Then, I realized
-    I wasn't the only one who has faced such moments, so I decided to make it public.
-    Now, anyone can share their experiences here, and the best part is that it's
-    completely anonymous—no one is watching you.
+  <div class="w-10/12 h-full py-24 text-white">
+    <h1 class="font-bold text-2xl uppercase">About this app</h1>
+
+    <div class="w-1/2 h-full text-justify text-lg font-normal mt-4 backdrop-blur-sm">
+      This is a <a href="https://svelte.dev/docs/kit">SvelteKit</a> app. I created
+      it for people who want to share their terrible experiences—whether about their
+      day or any situation. Initially, I made this app for myself when I was having
+      a horrible day and regretted being too shy to express my feelings. Then, I
+      realized I wasn't the only one who has faced such moments, so I decided to
+      make it public. Now, anyone can share their experiences here, and the best
+      part is that it's completely anonymous—no one is watching you.
+    </div>
   </div>
+
 </div>
--- src/routes/confession/+page.svelte
+++ src/routes/confession/+page.svelte
@@ -2,10 +2,14 @@
   import { onMount } from "svelte";
   import { theme } from "../../stores/theme";
 
+  // Icons Import
   import { Icon } from "svelte-icons-pack";
   import { AiTwotoneHeart } from "svelte-icons-pack/ai";
   import { BiBookmark } from "svelte-icons-pack/bi";
   import { BiComment } from "svelte-icons-pack/bi";
+
+  // Images Import
+  import post2 from "$lib/images/002.jpg";
 
   let toggleCommentVisibility: boolean[] = [];
 
@@ -33,91 +37,99 @@
 </script>
 
 <svelte:head>
-  <title>About</title>
-  <meta name="description" content="About this app" />
+  <title>Confessions</title>
+  <meta name="description" content="All Confessions" />
 </svelte:head>
 
-<section class="flex-1 flex flex-col justify-center items-center w-full">
-  <div>
-    {#if error}
-      <p>Error: {error}</p>
-    {:else if posts.length === 0}
-      <div class="pyramid-loader">
-        <div class="wrapper">
-          <span class="side side1"></span>
-          <span class="side side2"></span>
-          <span class="side side3"></span>
-          <span class="side side4"></span>
-          <span class="shadow"></span>
-        </div>
-      </div>
-    {:else}
-      <div class="flex flex-col w-auto gap-4">
-        {#each posts as item, index}
-          <div
-            class={`w-96 shadow p-4 relative ${$theme == "dark" ? "bg-[#eaeaea] text-[#1a1a1a]" : "bg-[#1a1a1a] text-[#eee]"}`}
-          >
-            <h2 class="capitalize">By, {item.username}</h2>
-            <div class="mt-2 mb-6">{item.confession}</div>
+<div class="flex flex-col items-center justify-start w-full h-full">
+  <div class="w-10/12 h-full py-24 text-white">
+    <h1 class="font-bold text-2xl uppercase">Confessions</h1>
 
-            <span class="absolute right-0 top-0 px-3 py-1 shadow"
-              >{item.type}</span
-            >
-
-            <div class="flex flex-row gap-4">
-              <div class="flex flex-col items-center justify-center">
-                <Icon src={AiTwotoneHeart} className="w-6 h-6 cursor-pointer" />
-                {item.likes}
-              </div>
-
-              <div class="flex flex-col items-center justify-center">
-                <Icon src={BiBookmark} className="w-6 h-6 cursor-pointer" />
-                {item.saves}
-              </div>
-
-              <button
-                type="button"
-                class="flex flex-col items-center justify-center"
-                on:click={() => toggleComments(index)}
-                aria-expanded={toggleCommentVisibility[index]}
-                aria-label={`Toggle comments for ${item.username}'s post`}
-              >
-                <Icon src={BiComment} className="w-6 h-6" />
-                {item.comments.length}
-              </button>
+    <div class="w-10/12 h-full mt-4">
+      <div class="flex flex-row justify-start items-center w-10/12">
+        {#if error}
+          <p>Error: {error}</p>
+        {:else if posts.length === 0}
+          <div class="pyramid-loader">
+            <div class="wrapper">
+              <span class="side side1"></span>
+              <span class="side side2"></span>
+              <span class="side side3"></span>
+              <span class="side side4"></span>
+              <span class="shadow"></span>
             </div>
-
-            {#if toggleCommentVisibility[index]}
-              <div class="mt-4 border-t pt-2">
-                <h3 class="text-lg font-semibold">Comments</h3>
-                <ul>
-                  {#each item.comments as comment}
-                    <li class="mt-2">
-                      <strong>{comment.username}:</strong>
-                      {comment.message}
-                      {#if comment.replies.length > 0}
-                        <ul class="ml-4">
-                          {#each comment.replies as reply}
-                            <li>
-                              <strong>{reply.username}:</strong>
-                              {reply.message}
-                            </li>
-                          {/each}
-                        </ul>
-                      {/if}
-                    </li>
-                  {/each}
-                </ul>
-              </div>
-            {/if}
-
-            <!-- <p>
-              <strong>Created At:</strong>
-              {new Date(item.createdAt).toLocaleString()}
-            </p> -->
           </div>
-        {/each}
+        {:else}
+          <div class="flex flex-row flex-wrap w-full gap-6">
+            {#each posts as item, index}
+              <div class="flex flex-col w-auto gap-4">
+                <div class="text-white font-bold text-lg">#0{index + 1}</div>
+                <img
+                  src={post2}
+                  alt=""
+                  class="w-72 h-40 rounded-lg shadow object-cover"
+                />
+                <div class={`w-full relative text-white`}>
+                  <h2 class="uppercase font-semibold text-base">
+                    By, {item.username}
+                  </h2>
+                  <div class="text-base font-light">{item.confession}</div>
+                </div>
+                <hr class="w-full bg-white" />
+
+                <!-- likes, saves and comments -->
+                <div class="flex flex-row gap-4">
+                  <div class="flex flex-row gap-2 items-center justify-center">
+                    <Icon
+                      src={AiTwotoneHeart}
+                      className="w-6 h-6 cursor-pointer"
+                    />
+                    {item.likes}
+                  </div>
+                  <div class="flex flex-row gap-2 items-center justify-center">
+                    <Icon src={BiBookmark} className="w-6 h-6 cursor-pointer" />
+                    {item.saves}
+                  </div>
+                  <button
+                    type="button"
+                    class="flex flex-row gap-2 items-center justify-center"
+                    on:click={() => toggleComments(index)}
+                    aria-expanded={toggleCommentVisibility[index]}
+                    aria-label={`Toggle comments for ${item.username}'s post`}
+                  >
+                    <Icon src={BiComment} className="w-6 h-6" />
+                    {item.comments.length}
+                  </button>
+                </div>
+
+                {#if toggleCommentVisibility[index]}
+                  <div class="border-t pt-2 w-72">
+                    <h3 class="text-lg font-semibold">Comments</h3>
+                    <ul>
+                      {#each item.comments as comment}
+                        <li class="mt-2">
+                          <strong>{comment.username}:</strong>
+                          {comment.message}
+                          {#if comment.replies.length > 0}
+                            <ul class="ml-4">
+                              {#each comment.replies as reply}
+                                <li>
+                                  <strong>{reply.username}:</strong>
+                                  {reply.message}
+                                </li>
+                              {/each}
+                            </ul>
+                          {/if}
+                        </li>
+                      {/each}
+                    </ul>
+                  </div>
+                {/if}
+              </div>
+            {/each}
+          </div>
+        {/if}
       </div>
-    {/if}
+    </div>
   </div>
-</section>
+</div>
